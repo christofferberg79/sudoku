42f0cb0b75bbfd2952e034fb1c275ad38485308c
Christoffer Berg <christoffer.berg@collectum.se>
2021-10-21 20:26:10 +0200

    solve last empty square: failing test

--- src/test/kotlin/SolverTest.kt
+++ src/test/kotlin/SolverTest.kt
@@ -11,4 +11,12 @@ class SolverTest {
         val result = solver.solve(input)
         assertEquals(input, result)
     }
+
+    @Test
+    fun lastEmptySquare() {
+        val input = "2564891733746159829817234565932748617128.6549468591327635147298127958634849362715"
+        val solver = Solver()
+        val result = solver.solve(input)
+        assertEquals("256489173374615982981723456593274861712836549468591327635147298127958634849362715", result)
+    }
 }
