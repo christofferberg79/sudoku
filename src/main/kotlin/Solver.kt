c8070f09517682e64d13353fbe3a6a314519b68b
Christoffer Berg <christoffer.berg@collectum.se>
2021-10-24 17:40:16 +0200

    solve hidden singles

--- src/main/kotlin/Solver.kt
+++ src/main/kotlin/Solver.kt
@@ -15,10 +15,14 @@ class Solver {
     private fun getOutput() = d.joinToString(separator = "")
 
     private fun set(i: Int, c: Char) {
-        d[i] = c
-        rowIndicesOf(i).forEach { possibles[it].remove(c) }
-        colIndicesOf(i).forEach { possibles[it].remove(c) }
-        blockIndicesOf(i).forEach { possibles[it].remove(c) }
+        if (c in '1'..'9') {
+            d[i] = c
+            rowIndicesOf(i).forEach { possibles[it].remove(c) }
+            colIndicesOf(i).forEach { possibles[it].remove(c) }
+            blockIndicesOf(i).forEach { possibles[it].remove(c) }
+            possibles[i].clear()
+            possibles[i].add(c)
+        }
     }
 
     private fun rowIndicesOf(i: Int): Iterable<Int> {
@@ -41,13 +45,38 @@ class Solver {
     private fun solve() {
         var progress = true
         while (progress) {
-            progress = false
-            d.forEachIndexed { i, c ->
-                if (c == '.' && possibles[i].size == 1) {
-                    set(i, possibles[i].single())
-                    progress = true
-                }
+            progress = nakedSingles()
+                    || hiddenSingles()
+        }
+    }
+
+    private fun nakedSingles(): Boolean {
+        d.forEachIndexed { i, c ->
+            if (c == '.' && possibles[i].size == 1) {
+                set(i, possibles[i].single())
+                return true
             }
         }
+        return false
+    }
+
+    private fun hiddenSingles(): Boolean {
+        for (group in groups()) {
+            for (c in '1'..'9') {
+                group.singleOrNull { i -> d[i] == '.' && c in possibles[i] }
+                    ?.let { i ->
+                        set(i, c)
+                        return true
+                    }
+            }
+        }
+        return false
+    }
+
+    private fun groups(): List<Iterable<Int>> {
+        val rows = (0..72 step 9).map { rowIndicesOf(it) }
+        val cols = (0..8).map { colIndicesOf(it) }
+        val blocks = listOf(0, 3, 6, 27, 30, 33, 54, 57, 60).map { blockIndicesOf(it) }
+        return rows + cols + blocks
     }
 }
