b582fb84ddbeab341c9a5286fd5f2b2cb09b003c
Christoffer Berg <christoffer.berg@collectum.se>
2021-11-09 14:18:54 +0100

    refactored Solver.kt

--- src/main/kotlin/Solver.kt
+++ src/main/kotlin/Solver.kt
@@ -5,23 +5,15 @@ class UniqueSolution(val solution: String) : Solution()
 object InvalidPuzzle : Solution()
 object TooHard : Solution()
 
+val symbols = '1'..'9'
+
 class Square(val row: Int, val col: Int) {
     val block: Int = row / 3 * 3 + col / 3
-
     var value: Char? = null
-        private set
-    val candidates = mutableSetOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
+    val candidates = symbols.toMutableSet()
 
     fun isSet() = value != null
     fun isNotSet() = value == null
-    fun set(c: Char) {
-        value = c
-        candidates.clear()
-    }
-
-    override fun toString(): String {
-        return value?.toString() ?: "."
-    }
 }
 
 class Solver {
@@ -62,34 +54,35 @@ class Solver {
         return true
     }
 
+    private fun insufficientGivens() = squares.count(Square::isSet) < 17
+
     private fun duplicateGivens() = groups().any { group ->
-        group.filter { it.isSet() }
-            .groupingBy { it.value }
+        group.filter(Square::isSet)
+            .groupingBy(Square::value)
             .eachCount()
             .values.any { it > 1 }
     }
-
-    private fun insufficientGivens() = squares.count(Square::isSet) < 17
 
     private fun noCandidate() = squares.any {
         it.isNotSet() && it.candidates.isEmpty()
     }
 
-    private fun missingCandidate() = ('1'..'9').any { c ->
+    private fun missingCandidate() = symbols.any { symbol ->
         groups().any { group ->
-            group.none { s -> c == s.value || c in s.candidates }
+            group.none { square -> symbol == square.value || symbol in square.candidates }
         }
     }
 
-    private fun setInput(input: String) = input.forEachIndexed { i, c -> set(squares[i], c) }
+    private fun setInput(input: String) = input.mapIndexed { i, c -> squares[i] to c }
+        .filter { (_, c) -> c in symbols }
+        .forEach { (s, c) -> set(s, c) }
 
-    private fun getOutput() = squares.joinToString(separator = "")
+    private fun getOutput() = squares.joinToString(separator = "") { s -> "${s.value ?: '.'}" }
 
-    private fun set(s: Square, c: Char) {
-        if (c in '1'..'9') {
-            s.set(c)
-            affectedBy(s).forEach { it.candidates.remove(c) }
-        }
+    private fun set(square: Square, symbol: Char) {
+        square.value = symbol
+        square.candidates.clear()
+        affectedBy(square).forEach { it.candidates.remove(symbol) }
     }
 
     private fun solve() {
@@ -101,23 +94,22 @@ class Solver {
     }
 
     private fun nakedSingles(): Boolean {
-        squares.forEach { s ->
-            if (s.candidates.size == 1) {
-                set(s, s.candidates.single())
+        squares.firstOrNull { square -> square.candidates.size == 1 }
+            ?.let { square ->
+                set(square, square.candidates.single())
                 return true
             }
-        }
         return false
     }
 
     private fun hiddenSingles(): Boolean {
         for (group in groups()) {
-            for (c in '1'..'9') {
-                val s = group.singleOrNull { s -> c in s.candidates }
-                if (s != null) {
-                    set(s, c)
-                    return true
-                }
+            for (symbol in symbols) {
+                group.singleOrNull { square -> symbol in square.candidates }
+                    ?.let { square ->
+                        set(square, symbol)
+                        return true
+                    }
             }
         }
         return false
@@ -130,5 +122,4 @@ class Solver {
     private fun affectedBy(s: Square): Sequence<Square> {
         return rows[s.row].asSequence() + cols[s.col].asSequence() + blocks[s.block].asSequence()
     }
-
 }
