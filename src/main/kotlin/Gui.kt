bfbdb6e1d2cdf3e378e22279dc38f63c703d7dce
Christoffer Berg <christofferberg79@gmail.com>
2021-11-13 16:51:22 +0100

    GUI: extracted state from composable

--- src/main/kotlin/Gui.kt
+++ src/main/kotlin/Gui.kt
@@ -25,10 +25,27 @@ fun gui() = singleWindowApplication(title = "Sudoku") {
     }
 }
 
+class GameState(val squares: List<Square>) {
+    constructor(input: String) : this(List(81) { i ->
+        require(input.length == 81)
+        require(input.all { c -> c == '.' || c in '1'..'9' })
+        val given = input[i] in '1'..'9'
+        val value = if (given) input[i] else null
+        Square(value, given)
+    })
+
+    data class Square(val value: Char?, val given: Boolean)
+
+    fun update(i: Int, char: Char?): GameState {
+        val newSquare = squares[i].copy(value = char)
+        val newSquares = squares.subList(0, i) + newSquare + squares.subList(i + 1, squares.size)
+        return GameState(newSquares)
+    }
+}
+
 @Composable
 private fun game(initialState: String) {
-    var state by remember { mutableStateOf(initialState) }
-    val given = remember { initialState.map { c -> c in '1'..'9' } }
+    var state by remember { mutableStateOf(GameState(initialState)) }
 
     Column {
         for (row in 0..8) {
@@ -36,10 +53,9 @@ private fun game(initialState: String) {
                 for (col in 0..8) {
                     val i = row * 9 + col
                     square(
-                        char = state[i],
-                        given = given[i],
+                        square = state.squares[i],
                         onTyped = { char ->
-                            state = state.substring(0, i) + char + state.substring(i + 1)
+                            state = state.update(i, char)
                         }
                     )
                 }
@@ -49,36 +65,39 @@ private fun game(initialState: String) {
 }
 
 @Composable
-fun square(char: Char, given: Boolean, onTyped: (Char) -> Unit) {
+fun square(square: GameState.Square, onTyped: (Char) -> Unit) {
     val focusRequester = remember { FocusRequester() }
     Box(
         modifier = Modifier
             .size(40.dp)
             .border(1.dp, Color.Black)
-            .run { if (given) {
-                this
-            } else {
-                this
-                    .focusRequester(focusRequester)
-                    .clickable { focusRequester.requestFocus() }
-                    .onKeyEvent { event ->
-                        if (event.nativeKeyEvent.id == KEY_PRESSED && event.nativeKeyEvent.keyChar in '1'..'9'
-                        ) {
-                            onTyped(event.nativeKeyEvent.keyChar)
-                            return@onKeyEvent true
-                        } else {
-                            return@onKeyEvent false
+            .run {
+                if (square.given) {
+                    this
+                } else {
+                    this
+                        .focusRequester(focusRequester)
+                        .clickable { focusRequester.requestFocus() }
+                        .onKeyEvent { event ->
+                            if (event.nativeKeyEvent.id == KEY_PRESSED &&
+                                event.nativeKeyEvent.keyChar in '1'..'9' &&
+                                event.nativeKeyEvent.keyChar != square.value
+                            ) {
+                                onTyped(event.nativeKeyEvent.keyChar)
+                                return@onKeyEvent true
+                            } else {
+                                return@onKeyEvent false
+                            }
                         }
-                    }
-            }
+                }
             }
     ) {
-        if (char in '1'..'9') {
+        if (square.value != null) {
             Text(
-                text = char.toString(),
+                text = square.value.toString(),
                 modifier = Modifier.align(Alignment.Center),
                 fontSize = 30.sp,
-                color = if (given) Color.Black else Color.Blue
+                color = if (square.given) Color.Black else Color.Blue
             )
         }
     }
