d8984ac163923bc19dd1f84c5f55dc6d8ce142d8
Christoffer Berg <christoffer.berg@collectum.se>
2021-11-25 10:02:42 +0100

    formatted code

--- src/main/kotlin/cberg/sudoku/solver/Technique.kt
+++ src/main/kotlin/cberg/sudoku/solver/Technique.kt
@@ -6,15 +6,20 @@ sealed interface Technique {
     class NakedSingle(private val position: Position, private val value: Char) : Technique {
         override fun toString() = "Naked Single $value at $position"
     }
+
     class HiddenSingle(private val position: Position, private val value: Char) : Technique {
         override fun toString() = "Hidden Single $value at $position"
     }
-    class NakedTuple(private val n: Int, private val positions: List<Position>, private val values: Set<Char>) : Technique {
+
+    class NakedTuple(private val n: Int, private val positions: List<Position>, private val values: Set<Char>) :
+        Technique {
         override fun toString(): String {
             return "Naked ${tupleString(n)} $values at $positions"
         }
     }
-    class HiddenTuple(private val n: Int, private val positions: List<Position>, private val values: Set<Char>) : Technique {
+
+    class HiddenTuple(private val n: Int, private val positions: List<Position>, private val values: Set<Char>) :
+        Technique {
         override fun toString(): String {
             return "Naked ${tupleString(n)} $values at $positions"
         }
@@ -23,7 +28,7 @@ sealed interface Technique {
 
 private fun tupleString(n: Int): String {
     require(n >= 2)
-    return when(n) {
+    return when (n) {
         2 -> "Pair"
         3 -> "Triple"
         else -> "$n-tuple"
