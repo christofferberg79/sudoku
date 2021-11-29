eeb8c4da3f2bd22a300440e952aa593aa0889f07
Christoffer Berg <christofferberg79@gmail.com>
2021-11-29 23:45:47 +0100

    GUI: refactored Square composable

--- src/main/kotlin/cberg/sudoku/gui/Gui.kt
+++ src/main/kotlin/cberg/sudoku/gui/Gui.kt
@@ -98,40 +98,30 @@ fun Game(
     onDelete: (Position) -> Unit
 ) {
     val focusRequesters = remember { game.squares.associate { square -> square.position to FocusRequester() } }
-    val interactionSource = remember { MutableInteractionSource() }
-    val focusManager = LocalFocusManager.current
 
     SudokuGrid(
         size = 468.dp,
         thickLine = BorderStroke(2.dp, Color.Black),
         thinLine = BorderStroke(1.dp, Color.Gray)
-    ) { row, col ->
-        val position = Position(row, col)
+    ) { position ->
         val focusRequester = focusRequesters.getValue(position)
-        var isFocused by remember { mutableStateOf(false) }
+        val focusManager = LocalFocusManager.current
         Square(
-            modifier = Modifier
-                .focusOrder(focusRequester) {
-                    up = focusRequesters.getValue(position.up())
-                    down = focusRequesters.getValue(position.down())
-                    left = focusRequesters.getValue(position.left())
-                    right = focusRequesters.getValue(position.right())
+            modifier = Modifier.focusOrder(focusRequester) {
+                up = focusRequesters.getValue(position.up())
+                down = focusRequesters.getValue(position.down())
+                left = focusRequesters.getValue(position.left())
+                right = focusRequesters.getValue(position.right())
+            },
+            square = game.squareAt(position),
+            onInput = { input ->
+                when (input) {
+                    is SquareInput.Value -> onType(position, input.value)
+                    is SquareInput.Delete -> onDelete(position)
+                    is SquareInput.Move -> focusManager.moveFocus(input.direction)
                 }
-                .background(if (isFocused) Color.LightGray else Color.White)
-                .onFocusChanged { focusState -> isFocused = focusState.hasFocus }
-                .clickable(interactionSource, indication = null) { focusRequester.requestFocus() }
-                .onKeyEvent { event ->
-                    when (val input = event.toSquareInput()) {
-                        is SquareInput.Value -> onType(position, input.value)
-                        is SquareInput.Delete -> onDelete(position)
-                        is SquareInput.Move -> focusManager.moveFocus(input.direction)
-                        else -> return@onKeyEvent false
-                    }
-                    return@onKeyEvent true
-                }
-                .fillMaxSize()
-                .padding(1.dp),
-            square = game.squareAt(position)
+            },
+            onClick = { focusRequester.requestFocus() }
         )
     }
 }
@@ -141,12 +131,12 @@ private fun SudokuGrid(
     size: Dp,
     thickLine: BorderStroke,
     thinLine: BorderStroke,
-    content: @Composable BoxScope.(row: Int, col: Int) -> Unit
+    content: @Composable BoxScope.(position: Position) -> Unit
 ) {
     Box(Modifier.size(size).background(thickLine.brush).padding(thickLine.width)) {
         Grid(thickLine) { outerRow, outerCol ->
             Grid(thinLine) { innerRow, innerCol ->
-                content(outerRow * 3 + innerRow, outerCol * 3 + innerCol)
+                content(Position(outerRow * 3 + innerRow, outerCol * 3 + innerCol))
             }
         }
     }
@@ -171,8 +161,28 @@ private fun Grid(
 }
 
 @Composable
-fun Square(modifier: Modifier = Modifier, square: Square) {
-    Box(modifier) {
+fun Square(
+    modifier: Modifier = Modifier,
+    square: Square,
+    onInput: (SquareInput) -> Unit,
+    onClick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    var isFocused by remember { mutableStateOf(false) }
+    Box(modifier
+        .onFocusChanged { focusState -> isFocused = focusState.hasFocus }
+        .clickable(interactionSource, indication = null) { onClick() }
+        .background(if (isFocused) Color.LightGray else Color.White)
+        .onKeyEvent { event ->
+            event.toSquareInput()?.let { input ->
+                onInput(input)
+                return@onKeyEvent true
+            }
+            return@onKeyEvent false
+        }
+        .fillMaxSize()
+        .padding(1.dp)
+    ) {
         if (square.isEmpty()) {
             SquareMarks(square)
         } else {
@@ -246,7 +256,7 @@ fun Hints(hints: Sequence<Hint>, onClick: (Hint) -> Unit) {
     }
 }
 
-private sealed class SquareInput {
+sealed class SquareInput {
     object Delete : SquareInput()
     data class Value(val value: Char) : SquareInput()
     data class Move(val direction: FocusDirection) : SquareInput()
