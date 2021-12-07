a2cf1fb1c34660b325676d75cdc34bbaf32e5799
Christoffer Berg <christofferberg79@gmail.com>
2021-12-07 21:54:48 +0100

    solve puzzles from file

--- src/main/kotlin/cberg/sudoku/Main.kt
+++ src/main/kotlin/cberg/sudoku/Main.kt
@@ -5,13 +5,15 @@ import cberg.sudoku.solver.InvalidPuzzle
 import cberg.sudoku.solver.Solver
 import cberg.sudoku.solver.TooHard
 import cberg.sudoku.solver.UniqueSolution
+import java.io.File
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        return gui()
-    }
-    if (args.size == 2 && args[0] == "solve") {
-        return solve(args[1])
+    when {
+        args.isEmpty() -> gui()
+        args.size == 2 && args[0] == "solve" -> solve(args[1])
+        args.size == 2 && args[0] == "file" -> solveFile(args)
     }
 }
 
@@ -21,5 +23,30 @@ private fun solve(input: String) {
         is InvalidPuzzle -> println("Invalid puzzle")
         is UniqueSolution -> println("Unique solution found: ${result.solution}")
         is TooHard -> println("Too hard")
+    }
+}
+
+@OptIn(ExperimentalTime::class)
+private fun solveFile(args: Array<String>) {
+    println("File: ${args[1]}")
+    File(args[1]).bufferedReader().use { reader ->
+        var count = 0
+        val solver = Solver()
+        val map = mutableMapOf<String, Int>()
+        val time = measureTime {
+            do {
+                reader.readLine()?.let { input ->
+                    if (input.matches(Regex("[1-9.]{81}"))) {
+                        count++
+                        val result = solver.solve(input)
+                        map.merge(result::class.simpleName!!, 1) { a, b -> a + b }
+                    }
+                } ?: break
+            } while (count < 1000)
+        }
+        println("Attempted $count puzzles in ${time.inWholeMilliseconds} ms")
+        for ((r, n) in map) {
+            println("%-20s %5d".format(r, n))
+        }
     }
 }
