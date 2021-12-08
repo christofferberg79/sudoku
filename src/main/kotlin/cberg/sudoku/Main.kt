53023acd40df34c28091da4a3012a9d006998cf0
Christoffer Berg <christofferberg79@gmail.com>
2021-12-08 23:16:17 +0100

    try to solve testfiles

--- src/main/kotlin/cberg/sudoku/Main.kt
+++ src/main/kotlin/cberg/sudoku/Main.kt
@@ -6,12 +6,18 @@ import cberg.sudoku.solver.Solver
 import cberg.sudoku.solver.TooHard
 import cberg.sudoku.solver.UniqueSolution
 import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.bufferedReader
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.listDirectoryEntries
+import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
 fun main(args: Array<String>) {
     when {
         args.isEmpty() -> gui()
+        args.size == 1 && args[0] == "testfiles" -> testfiles()
         args.size == 2 && args[0] == "solve" -> solve(args[1])
         args.size == 2 && args[0] == "file" -> solveFile(args)
     }
@@ -49,4 +55,62 @@ private fun solveFile(args: Array<String>) {
             println("%-20s %5d".format(r, n))
         }
     }
+}
+
+@OptIn(ExperimentalTime::class)
+fun testfiles() {
+    val solver = Solver()
+    val inputRegex = Regex("[1-9.]{81}")
+
+    println("File                                    Total  Attempts    Solved      %          Time")
+    println("--------------------------------------------------------------------------------------")
+    var total = 0
+    var attempts = 0
+    var solved = 0
+    var time = Duration.seconds(0)
+    val formatString = "%-35s%10d%10d%10d%7d %%%10d s"
+    Path("./test/data").listDirectoryEntries().filter { it.isRegularFile() }.forEach { file ->
+        var fileTotal = 0
+        var fileAttempts = 0
+        var fileSolved = 0
+        val fileTime = measureTime {
+            file.bufferedReader().useLines { lines ->
+                lines.filter { line -> line.matches(inputRegex) }
+                    .forEachIndexed { index, input ->
+                        fileTotal++
+                        if (index < 100000) {
+                            fileAttempts++
+                            if (solver.solve(input) is UniqueSolution) {
+                                fileSolved++
+                            }
+                        }
+                    }
+            }
+        }
+        println(
+            formatString.format(
+                file.fileName,
+                fileTotal,
+                fileAttempts,
+                fileSolved,
+                fileSolved * 100 / fileAttempts,
+                fileTime.inWholeSeconds
+            )
+        )
+        total += fileTotal
+        attempts += fileAttempts
+        solved += fileSolved
+        time += fileTime
+    }
+    println("--------------------------------------------------------------------------------------")
+    println(
+        formatString.format(
+            "Total",
+            total,
+            attempts,
+            solved,
+            solved * 100 / attempts,
+            time.inWholeSeconds
+        )
+    )
 }
